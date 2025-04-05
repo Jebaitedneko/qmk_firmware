efd7fcf731ebbf6eaaba274d8952f51863806a00
Jebaitedneko <Jebaitedneko@gmail.com>
2025-04-05 19:43:16 +0530

    qmk: init osu keypad

---
+++ keyboards/osupico/config.h
@@ -0,0 +1,26 @@
+#define USB_POLLING_INTERVAL_MS 1
+#define DEBOUNCE 1
+#define MATRIX_IO_DELAY 0
+#define TAPPING_TERM 0
+#define TAPPING_TERM_PER_KEY 0
+#define GPIO_INPUT_PIN_DELAY 0
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif
+
+#ifndef NO_ACTION_LAYER
+#define NO_ACTION_LAYER
+#endif
+
+#ifndef NO_ACTION_TAPPING
+#define NO_ACTION_TAPPING
+#endif
+
+#ifndef NO_ACTION_ONESHOT
+#define NO_ACTION_ONESHOT
+#endif
+
+#ifndef NO_MUSIC_MODE
+#define NO_MUSIC_MODE
+#endif
